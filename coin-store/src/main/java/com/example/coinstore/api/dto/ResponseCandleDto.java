3df99de25c37932e120d78d99eca82aa184c37dd
areztsov <areztsov@neoflex.ru>
2022-02-19 15:30:46 +0300

    1234

--- coin-store/src/main/java/com/example/coinstore/api/dto/ResponseCandleDto.java
+++ coin-store/src/main/java/com/example/coinstore/api/dto/ResponseCandleDto.java
@@ -1,9 +1,16 @@
 package com.example.coinstore.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseCandleDto {
 
     UUID id;
